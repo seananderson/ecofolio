eb637cf46ae91260b0aae6f8224a0c4ef8f9a461
Sean Anderson <github@seananderson.ca>
2013-05-06 18:25:55 -0700

    Add pinkbr data

---
+++ R/pinkbr.r
@@ -0,0 +1,26 @@
+#' Pink salmon data from the Broughton Archipelago, British Columbia,
+#' Canada from Krkosek et al. (2011) PNAS.
+#'
+#' These data represent pink salmon recruits for even years in the
+#' Broughton Archipelago, BC, Canada. They are available in the
+#' supplementary material of Krkosek et al. (2011). There were
+#' originally provided by Fisheries and Oceans Canada. Column headers
+#' denote river names.
+#'
+#' @name pinkbr
+#' @docType data
+#' @keywords data
+#' @references 
+#' Krkosek, M., Connors, B.M., Morton, A., Lewis, M.A., Dill, L.M. &
+#' Hilborn, R. (2011). Effects of parasites from salmon farms on
+#' productivity of wild salmon. Proceedings of the National Academy of
+#' Sciences of the United States of America, 108, 14700-14704.
+#'
+#' Ford, J. & Myers, R.A. (2008). A Global Assessment of Salmon
+#' Aquaculture Impacts on Wild Salmonids. PLoS Biology, 6(2), e33
+#'
+#' Krkosek, M., Ford, J.S., Morton, A., Lele, S., Myers, R.A. & Lewis,
+#' M.A. (2007). Declining Wild Salmon Populations in Relation to
+#' Parasites from Farm Salmon. Science, 318, 1772–1775.
+NULL
+
