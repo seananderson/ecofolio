28a32bbe5a0646785d4b56cfbd1b76ed7214e83d
Sean Anderson <github@seananderson.ca>
2013-05-12 17:34:04 -0700

    Refactor pe_mv to deal with prediction under import namespaces

--- R/pe_mv.r
+++ R/pe_mv.r
@@ -30,7 +30,6 @@
 #' be calculated and returned (defaults to \code{TRUE}).
 #' @param na.rm A logical value indicating whether \code{NA} values should be
 #' row-wise deleted. 
-#'   
 #'
 #' @return A numeric value representing the portfolio effect that
 #' takes into account the mean-variance relationship. If confidence
@@ -66,19 +65,22 @@
 
 # TODO move predictions within model type to deal with robustbase name space
 
-pe_mv <- function(x, fit_type = c("linear", "linear_robust", "quadratic",
+pe_mv <- function(x, type = c("linear", "linear_robust", "quadratic",
   "linear_quad_avg",  "linear_detrended", "loess_detrended"), ci =
     FALSE, na.rm = FALSE) {
   
-  fit_type <- fit_type[1]
+  type <- type[1]
   
-  if(!fit_type %in% c("linear", "linear_robust", "quadratic", "linear_quad_avg", "linear_detrended", "loess_detrended")) 
-    stop("not a valid fit_type")
+  if(!type %in% c("linear", "linear_robust", "quadratic",
+      "linear_quad_avg", "linear_detrended", "loess_detrended")){
+    stop("not a valid type")
+  }
   
-  if(!fit_type %in% c("linear", "linear_detrended", "loess_detrended")){
- #   if(ci == TRUE | boot == TRUE){
+  if(!type %in% c("linear", "linear_robust", "linear_detrended",
+      "loess_detrended")){
     if(ci == TRUE){
-      warning("Confidence intervals aren't supported for this type of mean-variance model. Setting ci = FALSE.")
+      warning("Confidence intervals aren't supported for this type of
+        mean-variance model. Setting ci = FALSE.")
     }
     ci <- FALSE
   }
@@ -86,15 +88,16 @@ pe_mv <- function(x, fit_type = c("linear", "linear_robust", "quadratic",
   if(na.rm) x <- na.omit(x)
   
   total_nas <- sum(is.na(x))
-  ifelse(!na.rm & total_nas > 0, return_na <- TRUE, return_na <- FALSE)
+  return_na <- ifelse(!na.rm & total_nas > 0, TRUE, FALSE)
   
   ## first get the means:
   m <- apply(x, 2, mean)
   single_asset_mean <- mean(rowSums(x))
+
   cv_portfolio <- cv(rowSums(x))
   
   ## now detrend if desired:
-  if(fit_type == "linear_detrended") {
+  if(type == "linear_detrended") {
     ## first get cv of detrended portfolio abundance:
     sd_portfolio <- sd(residuals(lm(rowSums(x)~c(1:nrow(x)))))
     mean_portfolio <- mean(rowSums(x))
@@ -102,7 +105,7 @@ pe_mv <- function(x, fit_type = c("linear", "linear_robust", "quadratic",
     ## now detrend:
     x <- apply(x, 2, function(y) residuals(lm(y~c(1:length(y)))))
   }
-  if(fit_type == "loess_detrended") {
+  if(type == "loess_detrended") {
     ## first get CV of detrended portfolio abundance:
     sd_portfolio <- sd(residuals(loess(rowSums(x)~c(1:nrow(x)))))
     mean_portfolio <- mean(rowSums(x))
@@ -113,59 +116,66 @@ pe_mv <- function(x, fit_type = c("linear", "linear_robust", "quadratic",
   
   ## and get the variances for the assets:
   v <- apply(x, 2, var)
-  
+
   log.m <- log(m)
   log.v <- log(v)
   d <- data.frame(log.m = log.m, log.v = log.v, m = m, v = v)
   
-  taylor_fit <- switch(fit_type[1], 
-    linear =  lm(log.v ~ log.m, data = d),
+  taylor_fit <- switch(type[1], 
+    linear = {
+      lm(log.v ~ log.m, data = d)
+    },
     linear_robust = {
       robustbase::lmrob(log.v ~ log.m, data = d)
     },
-    quadratic = nls(log.v ~ B0 + B1 * log.m + B2 * I(log.m ^ 2), data
-      = d, start = list(B0 = 0, B1 = 2, B2 = 0), lower = list(B0 =
-          -1e9, B1 = 0, B2 = 0), algorithm = "port"),
-    linear_detrended = lm(log.v ~ log.m, data = d),
-    loess_detrended = lm(log.v ~ log.m, data = d),
+    quadratic = {
+      nls(log.v ~ B0 + B1 * log.m + B2 * I(log.m ^ 2),
+        data = d, start = list(B0 = 0, B1 = 2, B2 = 0), lower =
+        list(B0 = -1e9, B1 = 0, B2 = 0), algorithm = "port")
+    },
+    linear_detrended = {
+      lm(log.v ~ log.m, data = d)
+    }, 
+    loess_detrended = {
+      lm(log.v ~ log.m, data = d)
+    },
     linear_quad_avg = {
-      linear = nls(log.v ~ B0 + B1 * log.m, data = d, start = list(B0 =
+      linear <- nls(log.v ~ B0 + B1 * log.m, data = d, start = list(B0 =
           0, B1 = 2), lower = list(B0 = -1e9, B1 = 0), algorithm =
           "port")
-      quadratic = nls(log.v ~ B0 + B1 * log.m + B2 * I(log.m ^ 2), data
+      quadratic <- nls(log.v ~ B0 + B1 * log.m + B2 * I(log.m ^ 2), data
         = d, start = list(B0 = 0, B1 = 2, B2 = 0), lower = list(B0 =
             -1e9, B1 = 0, B2 = 0), algorithm = "port")
-      avg.mod <- MuMIn::model.avg(list(linear=linear, quad=quadratic), rank = AICc)
-      avg.mod
+      MuMIn::model.avg(list(linear=linear, quad=quadratic), rank = MuMIn::AICc)
     }
   )
-  
-  if(ci == TRUE){
-  single_asset_variance_predict <- predict(taylor_fit, newdata =
+
+  if(ci) {
+    single_asset_variance_predict <- predict(taylor_fit, newdata =
       data.frame(log.m = log(single_asset_mean)), se = TRUE)
-  single_asset_variance <- exp(single_asset_variance_predict$fit)
-  }else{
-  single_asset_variance_predict <- predict(taylor_fit, newdata =
-      data.frame(log.m = log(single_asset_mean)))
-    single_asset_variance <- exp(as.numeric(single_asset_variance_predict))
+    single_asset_variance <- exp(single_asset_variance_predict$fit)
+  } else {
+    single_asset_variance_predict <- predict(taylor_fit, newdata =
+      data.frame(log.m = log(single_asset_mean)), se = FALSE)
+    single_asset_variance <- exp(single_asset_variance_predict)
   }
+  
   cv_single_asset <- sqrt(single_asset_variance) / single_asset_mean
   pe <- as.numeric(cv_single_asset / cv_portfolio)
   
   if(ci == TRUE) {
-    single_asset_variance_ci <- exp(single_asset_variance_predict$fit + c(-1.96, 1.96) * single_asset_variance_predict$se.fit)
+    single_asset_variance_ci <- exp(single_asset_variance_predict$fit
+      + c(-1.96, 1.96) * single_asset_variance_predict$se.fit)
     cv_single_asset_ci <- sqrt(single_asset_variance_ci) / single_asset_mean
     pe_ci <- as.numeric(cv_single_asset_ci / cv_portfolio)
     pe_ci <- pe_ci[order(pe_ci)] # make sure the lower value is first
     out <- list(pe = pe, ci = pe_ci)
-  }
-  
-  if(ci == FALSE) {
+  } else {
     out <- pe
   }
-  
+
   if(return_na) out <- NA
-  
+
   out
 }
 
