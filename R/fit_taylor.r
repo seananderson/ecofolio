f5407124181d2c8ee71d21d579e5af1c5271fcf0
Sean Anderson <github@seananderson.ca>
2013-07-02 19:23:45 -0700

    Re-wrap documentation

--- R/fit_taylor.r
+++ R/fit_taylor.r
@@ -15,24 +15,27 @@
 #' were requested then the list will also contain \code{ci} with the
 #' 95\% confidence intervals on the z value.
 #'
-#' @param x A matrix or dataframe of abundance or biomass data. The columns
-#' should represent different subpopulations or species. The rows should
-#' represent the values through time.
-#' @param ci A Logical value indicating whether 95\% confidence intervals should
-#' be calculated for the z value (the exponent in Taylor's power law).
-#' @param na.rm A logical value indicating whether \code{NA} values should be
-#' row-wise deleted. 
+#' @param x A matrix or dataframe of abundance or biomass data. The
+#' columns should represent different subpopulations or species. The
+#' rows should represent the values through time.
+#' @param ci A Logical value indicating whether 95\% confidence
+#' intervals should be calculated for the z value (the exponent in
+#' Taylor's power law).
+#' @param na.rm A logical value indicating whether \code{NA} values
+#' should be row-wise deleted. 
 #'
 #' @references
-#' Taylor, L. 1961. Aggregation, Variance and the Mean. Nature 189:732-735. doi:
-#' 10.1038/189732a0.
+#' Taylor, L. 1961. Aggregation, Variance and the Mean. Nature
+#' 189:732-735. doi: 10.1038/189732a0.
 #'
-#' Taylor, L., I. Woiwod, and J. Perry. 1978. The Density-Dependence of Spatial
-#' Behaviour and the Rarity of Randomness. J. Anim. Ecol. 47:383-406.
+#' Taylor, L., I. Woiwod, and J. Perry. 1978. The Density-Dependence
+#' of Spatial Behaviour and the Rarity of Randomness. J. Anim. Ecol.
+#' 47:383-406.
 #'
 #' Taylor, L., and I. Woiwod. 1982. Comparative Synoptic Dynamics. I.
-#' Relationships Between Inter- and Intra-Specific Spatial and Temporal
-#' Variance/Mean Population Parameters. J. Anim. Ecol. 51:879-906.
+#' Relationships Between Inter- and Intra-Specific Spatial and
+#' Temporal Variance/Mean Population Parameters. J. Anim. Ecol.
+#' 51:879-906.
 #' @export
 #' @examples
 #' data(pinkbr)
--- R/pe_avg_cv.r
+++ R/pe_avg_cv.r
@@ -1,42 +1,45 @@
 #' Estimate the average-CV portfolio effect
 #' 
-#' Takes a matrix of abundance or biomass data and returns various estimates of
-#' the average-CV portfolio effect. Options exist to detrend the time series
-#' data.
+#' Takes a matrix of abundance or biomass data and returns various
+#' estimates of the average-CV portfolio effect. Options exist to
+#' detrend the time series data.
 #' 
 #' @references 
-#' Doak, D., D. Bigger, E. Harding, M. Marvier, R. O'Malley, and D. Thomson.
-#' 1998. The Statistical Inevitability of Stability-Diversity Relationships in
-#' Community Ecology. Amer. Nat. 151:264-276.
+#' Doak, D., D. Bigger, E. Harding, M. Marvier, R. O'Malley, and D.
+#' Thomson. 1998. The Statistical Inevitability of Stability-Diversity
+#' Relationships in Community Ecology. Amer. Nat. 151:264-276.
 #' 
 #' Tilman, D., C. Lehman, and C. Bristow. 1998. Diversity-Stability
-#' Relationships: Statistical Inevitability or Ecological Consequence? Amer.
-#' Nat. 151:277-282.
+#' Relationships: Statistical Inevitability or Ecological Consequence?
+#' Amer. Nat. 151:277-282.
 #' 
-#' Schindler, D., R. Hilborn, B. Chasco, C. Boatright, T. Quinn, L. Rogers, and
-#' M. Webster. 2010. Population diversity and the portfolio effect in an
-#' exploited species. Nature 465:609-612. doi: 10.1038/nature09060.
+#' Schindler, D., R. Hilborn, B. Chasco, C. Boatright, T. Quinn, L.
+#' Rogers, and M. Webster. 2010. Population diversity and the
+#' portfolio effect in an exploited species. Nature 465:609-612. doi:
+#' 10.1038/nature09060.
 #' 
-#' @details This version of the portfolio effect consists of dividing the mean
-#' of the coefficient of variations (CV) of all individual subpopulations
-#' (assets) by the CV of the combined total population.
+#' @details This version of the portfolio effect consists of dividing
+#' the mean of the coefficient of variations (CV) of all individual
+#' subpopulations (assets) by the CV of the combined total population.
 #'   
-#' @param x A matrix or dataframe of abundance or biomass data. The columns
-#' should represent different subpopulations or species. The rows should
-#' represent the values through time.
-#' @param detrending Character value describing if (and how) the time series
-#' should be detrended before estimating the portfolio effect. Defaults to not
-#' detrending.
-#' @param ci Logical value (defaults to \code{FALSE}). Should a 95\% confidence
-#' interval be calculated using a bootstrap procedure? Returns the
-#' bias-corrected (bca) version of the bootstrap confidence interval.
+#' @param x A matrix or dataframe of abundance or biomass data. The
+#' columns should represent different subpopulations or species. The
+#' rows should represent the values through time.
+#' @param detrending Character value describing if (and how) the time
+#' series should be detrended before estimating the portfolio effect.
+#' Defaults to not detrending.
+#' @param ci Logical value (defaults to \code{FALSE}). Should a 95\%
+#' confidence interval be calculated using a bootstrap procedure?
+#' Returns the bias-corrected (bca) version of the bootstrap
+#' confidence interval.
 #' @param boot_reps Number of bootstrap replicates.
-#' @param na.rm A logical value indicating whether \code{NA} values should be
-#' row-wise deleted. 
+#' @param na.rm A logical value indicating whether \code{NA} values
+#' should be row-wise deleted. 
 #'   
-#' @return A numeric value representing the average-CV portfolio effect. If
-#' confidence intervals were requested then a list is returned with the
-#' portfolio effect \code{pe} and 95\% bootstrapped confidence interval
+#' @return A numeric value representing the average-CV portfolio
+#' effect. If confidence intervals were requested then a list is
+#' returned with the portfolio effect \code{pe} and 95\% bootstrapped
+#' confidence interval
 #' \code{ci}.
 #' 
 #' @examples 
@@ -109,3 +112,4 @@ pe_avg_cv <- function(x, detrending = c("not_detrended", "linear_detrended",
   if(return_na) out <- NA
   out
 }
+
